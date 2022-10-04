ed6d342a87d259fa3d8d4320956f277a7a55aa19
rainbowjess <jessica.m.harrison11@gmail.com>
2022-10-03 20:42:04 -0400

    Animation commit

--- Fall2022SoundAnim/Assets/Scripts/CatController.cs
+++ Fall2022SoundAnim/Assets/Scripts/CatController.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
-public AudioClip musicClipOne;
+    public AudioClip musicClipOne;
 
-public AudioClip musicClipTwo;
+    public AudioClip musicClipTwo;
 
-public AudioSource musicSource;
+    public AudioSource musicSource;
+
+    Animator anim;
+
+    void Start ()
+    {
+       anim = GetComponent<Animator>();
+    }
+
 
 // Update is called once per frame
 void Update()
@@ -17,12 +25,14 @@ void Update()
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
 
          }
 
@@ -30,11 +40,14 @@ void Update()
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
+        
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
 
          }
 
